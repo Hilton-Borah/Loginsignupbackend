688aafaafd4f54ab3337fca7b74ea04434a3aec3
Hilton Borah <hiltonborah123@gmail.com>
2023-01-28 00:23:09 +0530

    main login

---
+++ index.js
@@ -0,0 +1,23 @@
+require("./config/db")
+require('dotenv').config()
+const express = require("express");
+const { userRotes } = require("./Routes/userRoute");
+const cors = require("cors");
+const app = express();
+
+app.use(cors({
+    origin:"*"
+}))
+app.use(express.json());
+app.use("/user",userRotes)
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server running on port ${process.env.PORT}`)
+})
+
+
+// "name":"halje",
+// "email":"hiltonborah123@gmail.com",
+// "password":"Abc123@",
+// "dateOfBirth":"85478"
